6fdef48fcd46645bff7204521a63490243a3c78f
Rohit Prajapati <rohitpjpti18@gmail.com>
2021-05-14 09:02:31 +0530

    update wallpaper

--- js/main.js
+++ js/main.js
@@ -3,19 +3,29 @@ function sleep(){
 }
 
 let welcome = document.getElementById("typewriter");
+let ase = document.getElementById("ase"); 
 
-async function load(){
-    let msg = 'I am Rohit Prajapati.';
-    
+async function typeWriterEffect(msg, element){
     await sleep();
     for(let i = 0; i<msg.length; i++){
         if(msg[i] == ' '){
-            welcome.innerHTML += ' ';
+            element.innerHTML += ' ';
         }else{
-            welcome.innerHTML += msg[i];
+            element.innerHTML += msg[i];
         }
         await sleep();
     }
+}
+
+async function load(){
+    let msg1 = `I'm Rohit Prajapati.`;
+    
+    let msg2 = `a software engineer`;
+
+    await typeWriterEffect(msg1, welcome);
+    await typeWriterEffect(msg2, ase);
+    
+
 
 }
 
@@ -28,7 +38,7 @@ function myFunction() {
         navbar.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
     } 
     else {
-        navbar.className = "navbar navbar-expand-lg navbar-light fixed-top";
+        navbar.className = "navbar navbar-expand-lg navbar-dark fixed-top";
         navbar.style.boxShadow = "";
     }
 }
